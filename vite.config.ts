f011e6c75fdb60f26678022938a17873c56f21bc
brunoholanda <holanda_rodrigues@hotmail.com>
2026-04-18 16:19:23 -0300

    notfound fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': { target: 'http://localhost:3000', changeOrigin: true },
-      '/uploads': { target: 'http://localhost:3000', changeOrigin: true },
+      '/api': { target: 'http://localhost:4055', changeOrigin: true },
+      '/uploads': { target: 'http://localhost:4055', changeOrigin: true },
     },
   },
 })
